eedea9a9814aaefd6ac0eb7b47d8e9f12ae5ab1d
SparksFullStack <csparks@antioch.edu>
2019-03-26 16:30:42 -0700

    file saving and loading functionality added

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -18,7 +18,21 @@ defmodule Cards do
   end
 
   def deal(deck, hand_size) do
-    Enum.split(deck, hand_size)
+    { hand, rest_of_deck } = Enum.split(deck, hand_size)
+    hand
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    { status, binary } = File.read(filename)
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "That file does not exist"
+    end
   end
 
 end
