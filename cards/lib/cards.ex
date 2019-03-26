f5f26d051ace8b41cc54f22174d5748963067a50
SparksFullStack <csparks@antioch.edu>
2019-03-26 15:16:13 -0700

    shuffle method added

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,5 +1,15 @@
 defmodule Cards do
-  def hello do
-    "Hi there!"
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def shuffle do
+    "howdy"
+  end
+
 end
